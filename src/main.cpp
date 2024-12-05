417a08524be9bcf224163aff3ccec8f17e941eb3
Max <maxboeer@users.noreply.github.com>
2024-12-05 03:36:26 +0100

    Added AI code comments & removed secrets

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,13 @@ Preferences preferences;
 
 #include "secrets.cpp"
 
+// Initialize SinricProBlinds objects with IDs from secrets
 SinricProBlinds &rightBlinds = SinricPro[secrets.sinric.right_blinds_id];
 SinricProBlinds &leftBlinds = SinricPro[secrets.sinric.left_blinds_id];
 
 int blindsPosition = 0;
 bool powerState = false;
 
-//const int LED_BUILTIN = 2;
 bool dir = true;
 unsigned int step_position_right = 0;
 unsigned int step_position_left = 0;
@@ -48,8 +48,6 @@ unsigned int iterations_right = 0;
 unsigned int iterations_left = 0;
 double interpolation_length = 500;
 double interpolation_factor = log10(steptime_low/steptime_high) / (interpolation_length * log10(double(1)/double(2)));
-//double falling_factor = log10f(steptime_high/steptime_low) / (double(100) * log10f(double(3)/double(2)));
-
 
 unsigned long current_micros = micros();
 unsigned long step_micros_right = micros();
@@ -57,27 +55,31 @@ unsigned long step_micros_left = micros();
 unsigned long previousmicrosWifi = micros();
 unsigned long previousmicrosDot = micros();
 unsigned long rssi_micros_prev = micros();
-const unsigned long  reconnectInterval = 1000*1000*15;
+const unsigned long reconnectInterval = 1000*1000*15;
 unsigned int reconnect_counter = 0;
 
+// Function to restart the ESP device
 void Restart(){
     Serial.println("restarting in 5 Seconds");
+    // Save the current step positions to preferences
     if(preferences.getUInt("steps_right") != step_position_right)
         preferences.putUInt("steps_right", step_position_right);
     if(preferences.getUInt("steps_left") != step_position_left)
         preferences.putUInt("steps_left", step_position_left);
     preferences.end();
-    delay(5000);
+    delay(5000); // Wait for 5 seconds before restarting
     ESP.restart();
 }
 
+// Function to reconnect to WiFi
 void Reconnect(){
     while (WiFi.status() != WL_CONNECTED) {
         current_micros = micros();
 
+        // Check if the reconnect interval has passed
         if (current_micros - previousmicrosWifi >= reconnectInterval){
             if(reconnect_counter >= 10)
-                Restart();
+                Restart(); // Restart if too many reconnect attempts
 
             Serial.println("");
             Serial.print("Reconnecting");
@@ -87,6 +89,7 @@ void Reconnect(){
             previousmicrosWifi = current_micros;
         }
 
+        // Print a dot every 250ms to indicate reconnecting
         if (current_micros - previousmicrosDot >= 250000){
             Serial.printf(".");
             previousmicrosDot = current_micros;
@@ -94,25 +97,29 @@ void Reconnect(){
     }
 }
 
+// Event handler for WiFi disconnection
 void WiFiDisconnected(WiFiEvent_t event, WiFiEventInfo_t info){
     Reconnect();
 }
 
+// Callback for power state change
 bool onPowerState(const String &deviceId, bool &state) {
     Serial.printf("Device %s power turned %s \r\n", deviceId.c_str(), state?"on":"off");
     powerState = state;
     return true; // request handled properly
 }
 
+// Callback for setting absolute position of blinds
 bool onRangeValue(const String &deviceId, int &position) {
     Serial.println("Absolute");
 
-    if(deviceId == "63dc458d22e49e3cb5f7617b"){
+    if(deviceId == secrets.sinric.right_blinds_id){
+        // Calculate the delta steps needed to reach the desired position
         delta_steps_right = map(position, 0, 100, 0, top_steps) - step_position_right;
         step_position_right += delta_steps_right;
         preferences.putUInt("steps_right", step_position_right);
 
-    }else if(deviceId == "63dc45aa22e49e3cb5f761ab"){
+    }else if(deviceId == secrets.sinric.left_blinds_id){
         delta_steps_left += map(position, 0, 100, 0, top_steps) - step_position_left;
         step_position_left += delta_steps_left;
         preferences.putUInt("steps_left", step_position_left);
@@ -123,16 +130,18 @@ bool onRangeValue(const String &deviceId, int &position) {
     return true; // request handled properly
 }
 
+// Callback for adjusting position of blinds by a delta value
 bool onAdjustRangeValue(const String &deviceId, int &positionDelta) {
 
     Serial.println("Delta");
 
-    if(deviceId == "63dc458d22e49e3cb5f7617b"){
+    if(deviceId == secrets.sinric.right_blinds_id){
+        // Calculate the delta steps needed to adjust the position
         delta_steps_right = map(positionDelta, 0, 100, 0, top_steps);
         step_position_right += delta_steps_right;
         preferences.putUInt("steps_right", step_position_right);
 
-    }else if(deviceId == "63dc45aa22e49e3cb5f761ab"){
+    }else if(deviceId == secrets.sinric.left_blinds_id){
         delta_steps_left = map(positionDelta, 0, 100, 0, top_steps);
         step_position_left += delta_steps_left;
         preferences.putUInt("steps_left", step_position_left);
@@ -146,7 +155,7 @@ bool onAdjustRangeValue(const String &deviceId, int &positionDelta) {
     return true; // request handled properly
 }
 
-// setup function for WiFi connection
+// Setup function for WiFi connection
 void setupWiFi() {
     WiFi.disconnect(true);
     delay(1000);
@@ -161,24 +170,27 @@ void setupWiFi() {
     Serial.printf("connected!\r\n[WiFi]: IP-Address is %d.%d.%d.%d\r\n", localIP[0], localIP[1], localIP[2], localIP[3]);
 }
 
+// Setup function for SinricPro
 void setupSinricPro() {
-    // get a new Blinds device from SinricPro
+    // Register callbacks for right blinds
     rightBlinds.onPowerState(onPowerState);
     rightBlinds.onRangeValue(onRangeValue);
     rightBlinds.onAdjustRangeValue(onAdjustRangeValue);
 
-    // get a new Blinds device from SinricPro
+    // Register callbacks for left blinds
     leftBlinds.onPowerState(onPowerState);
     leftBlinds.onRangeValue(onRangeValue);
     leftBlinds.onAdjustRangeValue(onAdjustRangeValue);
 
-    // setup SinricPro
+    // Setup SinricPro connection
     SinricPro.onConnected([](){ Serial.printf("Connected to SinricPro\r\n"); });
     SinricPro.onDisconnected([](){ Serial.printf("Disconnected from SinricPro\r\n"); });
     SinricPro.begin(secrets.sinric.app_key, secrets.sinric.app_secret);
 }
 
+// Setup function for blinds
 void setupBlinds(){
+    // Retrieve the last known step positions from preferences
     step_position_right = preferences.getUInt("steps_right", 0);
     step_position_left = preferences.getUInt("steps_left", 0);
     pinMode(dir_pin1, OUTPUT);
@@ -190,18 +202,22 @@ void setupBlinds(){
     pinMode(powerOnPin, OUTPUT);
     pinMode(powerControlPin, INPUT_PULLUP);
 
+    // Initialize pins to default states
     digitalWrite(powerOnPin, 1);
     digitalWrite(step_pin1, 0);
     digitalWrite(step_pin2, 0);
     digitalWrite(LED_BUILTIN, false);
 }
 
+// Function to power on the blinds
 void powerOn(){
     digitalWrite(powerOnPin, false);
+    // Wait until the power control pin is high
     while (!digitalRead(powerControlPin)){
         digitalWrite(LED_BUILTIN, false);}
 }
 
+// Function to step the motor
 void step(){
     int *step_pin;
     int *dir_pin;
@@ -211,6 +227,7 @@ void step(){
     unsigned int *iterations;
     bool blinds_running = false;
 
+    // Determine which motor to step based on delta steps
     if(delta_steps_right != 0 && (!lastblinds || delta_steps_left == 0)){
         steptime = &steptime_right;
         if(current_micros - step_micros_right >= *steptime){
@@ -220,21 +237,18 @@ void step(){
             delta_steps_prev = &delta_steps_prev_right;
             iterations = &iterations_right;
             step_micros_right = current_micros;
-            //Serial.print("Right:");
             lastblinds = true;
             blinds_running = true;
         }
     }else if(delta_steps_left != 0 && (lastblinds || delta_steps_right == 0)){
         steptime = &steptime_left;
         if(current_micros - step_micros_left >= *steptime){
-            //Serial.println((double)(current_micros - step_micros_left));
             step_pin = &step_pin2;
             dir_pin = &dir_pin2;
             delta_steps = &delta_steps_left;
             delta_steps_prev = &delta_steps_prev_left;
             iterations = &iterations_left;
             step_micros_left = current_micros;
-            //Serial.print("Left:");
             lastblinds = false;
             blinds_running = true;
         }
@@ -245,6 +259,7 @@ void step(){
         return;
     }
 
+    // If blinds are running, calculate step time and step the motor
     if(blinds_running){
         if(abs(*delta_steps) + *iterations < interpolation_length*2)
             *steptime = steptime_high;
@@ -257,13 +272,6 @@ void step(){
 
         else if(abs(*delta_steps) <= interpolation_length)
             *steptime = steptime_high * pow((double(1)/double(2)), double(abs(*delta_steps)) * interpolation_factor);
-
-        //else
-        //  *steptime = steptime_low;
-
-        //Serial.print(*iterations);
-        //Serial.print(" | ");
-        //Serial.println(*steptime);
 
         powerOn();
 
@@ -282,9 +290,9 @@ void step(){
     }
 }
 
-// main setup function
+// Main setup function
 void setup() {
-    setCpuFrequencyMhz(240); //Set CPU clock to 80MHz fo example
+    setCpuFrequencyMhz(240); // Set CPU clock to 240MHz
 
     Serial.begin(secrets.sinric.baud_rate); Serial.printf("\r\n\r\n");
     preferences.begin("rollos", false);
@@ -295,13 +303,14 @@ void setup() {
     delay(500);
 }
 
+// Main loop function
 void loop() {
     SinricPro.handle();
 
     current_micros = micros();
     step();
 
-
+    // Print WiFi signal strength every 30 seconds
     if(WiFi.status() == WL_CONNECTED && current_micros - rssi_micros_prev >= 30000000){
         Serial.print("Connected Network Signal Strength (RSSI): ");
         Serial.println(WiFi.RSSI());  /*Print WiFi signal strength*/
